362f211201fcc2047c552e33c233a88b92a5e6fe
Houssam BEN OTHMAN <houssam.ben_othman@edu.devinci.fr>
2026-03-31 14:26:18 +0200

    Ajout de la classe Tour et finalisé

--- TourneeFutee/TourneeFutee/Tour.cs
+++ TourneeFutee/TourneeFutee/Tour.cs
@@ -1,39 +1,56 @@
-﻿namespace TourneeFutee
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TourneeFutee
 {
     // Modélise une tournée dans le cadre du problème du voyageur de commerce
     public class Tour
     {
         // TODO : ajouter tous les attributs que vous jugerez pertinents 
+        private float cost; // coût total de la tournée
+        private List<(string source, string destination)> segments; // liste des trajets de la tournée
 
+        public Tour()
+        {
+            cost = 0;
+            segments = new List<(string source, string destination)>();
+        }
         // propriétés
 
         // Coût total de la tournée
         public float Cost
         {
-            get;    // TODO : implémenter
+            get { return this.cost; }    // TODO : implémenter
         }
 
         // Nombre de trajets dans la tournée
         public int NbSegments
         {
-            get;    // TODO : implémenter
+            get { return this.segments.Count; }  // TODO : implémenter
         }
 
 
         // Renvoie vrai si la tournée contient le trajet `source`->`destination`
         public bool ContainsSegment((string source, string destination) segment)
         {
-            return false;   // TODO : implémenter 
+            return this.segments.Contains(segment);    // TODO : implémenter 
         }
 
 
         // Affiche les informations sur la tournée : coût total et trajets
         public void Print()
         {
-            // TODO : implémenter 
+            Console.WriteLine(ToString());// TODO : implémenter 
         }
 
         // TODO : ajouter toutes les méthodes que vous jugerez pertinentes 
-
+        public override string ToString()
+        {
+            string result = "Coût total : " + cost + "\nTrajets :\n";
+            foreach (var segment in segments)
+            {
+                result += segment.source + " -> " + segment.destination + "\n";
+            }
+            return result;
+        }
     }
 }
